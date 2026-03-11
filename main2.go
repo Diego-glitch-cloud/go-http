72468fd0bd5075e703a0b6f6b9b0313c6b630593
Diego-glitch-cloud <dcalderon918@gmail.com>
2026-03-11 10:19:15 -0600

    main2 api de bandas

--- main2.go
+++ main2.go
@@ -9,7 +9,7 @@ import (
 )
 
 
-type Banda struct {
+type Band struct {
 	ID int `json:"id"`
 	Name string `json:"name"`
 	Genre string `json:"genre"`
@@ -18,3 +18,18 @@ type Banda struct {
 	Members int `json:"members"`	// cantidad de miembros
 }
 
+type Message struct {
+	Message string `json:"message"`
+}
+
+var bands []Band
+
+func main() {
+	loadBandas()
+
+	http.HandleFunc("/api/ping", pingHandler)
+	http.HandleFunc("/api/bands", bandsHandler)
+
+	log.Println("POST JSON API running on :80")
+	log.Fatal(http.ListenAndServe(":80", nil))
+}
